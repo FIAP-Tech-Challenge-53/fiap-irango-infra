a0c342fc32171e4f88855f7f124b85b55626dd9c
Hélio Dantas <helio_dantas@outlook.com>
2024-05-06 10:54:08 -0300

    iniciando api gateway

--- src/api_gateway.tf
+++ src/api_gateway.tf
@@ -1,68 +1,40 @@
 # Crie um API Gateway
 resource "aws_api_gateway_rest_api" "fiap_gateway" {
-  name        = "fiap-gateway-api"
+  name        = "fiap_gateway-api"
   description = "Fiap API Gateway"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+    # vpc_endpoint_ids = [aws_vpc.default.id]
+  }
 }
 
 # Crie um recurso na API Gateway
-resource "aws_api_gateway_resource" "fiap-gateway_resource" {
+resource "aws_api_gateway_resource" "fiap_gateway_resource" {
   rest_api_id = aws_api_gateway_rest_api.fiap_gateway.id
   parent_id   = aws_api_gateway_rest_api.fiap_gateway.root_resource_id
-  path_part   = "fiap-gateway"
+  path_part   = "login"
 }
 
 # Defina um método na API Gateway (por exemplo, GET)
-resource "aws_api_gateway_method" "fiap-gateway_method" {
+resource "aws_api_gateway_method" "fiap_gateway_method" {
   rest_api_id   = aws_api_gateway_rest_api.fiap_gateway.id
-  resource_id   = aws_api_gateway_resource.fiap-gateway_resource.id
+  resource_id   = aws_api_gateway_resource.fiap_gateway_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-
-
-# Crie uma resposta para o método da API Gateway
-resource "aws_api_gateway_method_response" "fiap-gateway_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.fiap_gateway.id
-  resource_id = aws_api_gateway_resource.fiap-gateway_resource.id
-  http_method = aws_api_gateway_method.fiap-gateway_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Content-Type" = true
-  }
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
-# Crie uma resposta de integração para o método da API Gateway
-resource "aws_api_gateway_integration_response" "fiap-gateway_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.fiap_gateway.id
-  resource_id = aws_api_gateway_resource.fiap-gateway_resource.id
-  http_method = aws_api_gateway_method.fiap-gateway_method.http_method
-  status_code = aws_api_gateway_method_response.fiap-gateway_method_response.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-
-   depends_on = [
-    aws_api_gateway_method_response.fiap-gateway_method_response
-  ]
-}
 
 output "gateway_id" {
   value = aws_api_gateway_rest_api.fiap_gateway.id
 }
 
 output "gateway_resource_id" {
-  value = aws_api_gateway_resource.fiap-gateway_resource.id
+  value = aws_api_gateway_resource.fiap_gateway_resource.id
 }
 
 output "gateway_http_method" {
-  value = aws_api_gateway_method.fiap-gateway_method.http_method
+  value = aws_api_gateway_method.fiap_gateway_method.http_method
 }
 
 output "gateway_execution_arn" {
