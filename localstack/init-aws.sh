7e56d172da794d7a2e715740b2b0daa6574251a1
Hélio Dantas <helio_dantas@outlook.com>
2024-08-18 01:38:11 -0300

    feat: incluindo fila e sub para a cozinha ouvir a criação de pedidos

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -11,6 +11,7 @@ awslocal sns create-topic --name fiap-irango-cook_cooking-finished_dev
 
 # SQS
 awslocal sqs create-queue --queue-name fiap-irango-payment_order-created_dev
+awslocal sqs create-queue --queue-name fiap-irango-cook_order-created_dev
 awslocal sqs create-queue --queue-name fiap-irango-payment_gateway-postbacks_dev # TODO - Verificar se é necessário
 awslocal sqs create-queue --queue-name fiap-irango-order_payment-created_dev
 awslocal sqs create-queue --queue-name fiap-irango-order_payment-confirmed_dev
@@ -25,6 +26,13 @@ awslocal sns subscribe \
   --protocol sqs \
   --topic-arn arn:aws:sns:us-east-1:000000000000:fiap-irango-order_order-created_dev \
   --notification-endpoint arn:aws:sqs:us-east-1:000000000000:fiap-irango-payment_order-created_dev
+
+awslocal sns subscribe \
+  --endpoint-url=http://sqs.us-east-1.localhost.localstack.cloud:4566 \
+  --region us-east-1 \
+  --protocol sqs \
+  --topic-arn arn:aws:sns:us-east-1:000000000000:fiap-irango-order_order-created_dev \
+  --notification-endpoint arn:aws:sqs:us-east-1:000000000000:fiap-irango-cook_order-created_dev
 
 awslocal sns subscribe \
   --endpoint-url=http://sqs.us-east-1.localhost.localstack.cloud:4566 \
